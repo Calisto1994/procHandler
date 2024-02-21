d09d2988467e99105334511082a8b72742073fc9
Calisto1994 <ericfreiberg@outlook.de>
2024-02-21 13:54:56 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        while (true)
+                commandAsk();
+    }
+    public static void commandAsk() {
+        procHandler proc = new procHandler()
+                .waitFor(true);
+        String command = JOptionPane.showInputDialog(null, "Befehl:", "uname -a");
+        if (command == null) System.exit(1); // If cancel button is clicked, end program
+        try {
+            proc.shell_exec(command);
+        } catch (Exception ioe) {
+            System.out.println("Fehler!");
+        }
+
+        if (proc.pid != 0)  // Check if execution was successful
+            System.out.println(String.format("Process %d exited with code %d", proc.pid, proc.exitCode));
+        else
+            System.out.println("Execution error: No Process ID returned.");
+
+        if (!proc.stdOut.isEmpty())
+            JOptionPane.showMessageDialog(null, proc.stdOut, command, JOptionPane.INFORMATION_MESSAGE);
+        if (!proc.stdErr.isEmpty())
+            JOptionPane.showMessageDialog(null, proc.stdErr, command, JOptionPane.ERROR_MESSAGE);
+    }
+}
---
+++ src/procHandler.java
@@ -0,0 +1,81 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+ * Process Handler
+ *
+ * Allows PHP-like shell_exec() calls
+ * returns the stdOut, stdErr, pid and exitCode of the process that was executed.
+ *
+ */
+public class procHandler {
+    private boolean waitFor = false;
+    public String stdOut = new String();
+    public String stdErr = new String();
+    public long pid = 0;
+    public int exitCode = 0;
+
+    public procHandler shell_exec (String command) throws InterruptedException { // Run a command, return its stdOut
+        String[] results = {new String(), new String()};
+        String result = new String();
+        try {
+            Process process = Runtime.getRuntime().exec(procHandler.StrtoStrA(command));
+            if (waitFor) {   // Only if .waitFor(true) was declared, use a blocking process
+                process.waitFor();
+                this.exitCode = process.exitValue();
+            }
+
+            this.stdOut = scanStream(process.getInputStream());
+            this.stdErr = scanStream(process.getErrorStream());
+            this.pid = process.pid();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            this.pid = 0;
+            this.exitCode = 0;
+            this.stdOut = new String();
+            this.stdErr = "Error: Process execution failed.";
+        }
+        return(this);
+    }
+
+    // INPUTSTREAM OPERATIONS
+
+    public static String scanStream (InputStream inputStream) {
+        String result = new String();
+        Scanner scanner = new Scanner(inputStream);
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            if (line != null)
+                result += String.format("%s\n", line);
+        }
+        scanner.close();
+        return(result);
+    }
+
+    // STRING OPERATIONS
+
+    public static String StrAtoStr (String[] args) { // Converts a string array (like the command-line arguments) to a String.
+        String Out = new String(); // Initialize string
+        if (args.length > 0) { // Check if there's any arguments provided
+            for (int i = 0; i < args.length; i++) { // Convert through a "for"-Loop
+                Out += args[i] + " "; // concatenate arguments
+                if (i != args.length - 1) Out += " "; // Add a whitespace, if it's not the last argument
+            }
+
+        }
+        return(Out);
+    }
+
+    public static String[] StrtoStrA (String args) { // convert a String to a String Array (e.g. to use for shell_execute)
+        return args.split(" ");
+    }
+
+    // BUILDER OPERATIONS (allows for default values for certain parameters which may be called like .waitFor(true))
+    public procHandler waitFor (boolean waitFor) {
+        this.waitFor = waitFor;
+        return this;
+    }
+}
